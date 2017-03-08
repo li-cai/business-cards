aac8e16d892099cc53a29197243c9b481b136a19
Cailin Li <cxl2467@rit.edu>
2017-03-08 13:29:53 -0500

    Initial commit

---
+++ src/fileResponses.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const styleCSS = fs.readFileSync(`${__dirname}/../client/style.css`);
+const scriptsJS = fs.readFileSync(`${__dirname}/../client/scripts.js`);
+
+const char1Img = fs.readFileSync(`${__dirname}/../client/media/char1.png`);
+
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const getCSS = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/css' });
+  response.write(styleCSS);
+  response.end();
+};
+
+const getJS = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/javascript' });
+  response.write(scriptsJS);
+  response.end();
+};
+
+const getChar1Image = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(char1Img);
+  response.end();
+};
+
+module.exports = {
+  getIndex,
+  getCSS,
+  getJS,
+  getChar1Image,
+};
